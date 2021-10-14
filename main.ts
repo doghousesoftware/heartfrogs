348f58bd5729a32ed66459ffeeedab520e7528f6
bryan weinstein <bryanweinstein@outlook.com>
2021-10-14 14:42:35 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,52 @@
+input.onPinPressed(TouchPin.P0, function () {
+    radio.sendString("Weapons xReady")
+})
 radio.onReceivedNumber(function (receivedNumber) {
     numberCodeReceived(receivedNumber)
 })
 function numberCodeReceived (codeNumber: number) {
-    if (codeNumber == 7) {
-        serial.writeLine("HearFrogs")
+    if (codeNumber == 892) {
+        serialWriter(codeNumber, "")
+    } else if (codeNumber == 555) {
+        serialWriter(codeNumber, "")
+    } else if (codeNumber == 7) {
+        serialWriter(codeNumber, "")
     } else {
-        serial.writeLine("HearFrogs")
+        serialWriter(7, "")
     }
 }
 input.onButtonPressed(Button.A, function () {
-	
+    radio.sendNumber(892)
 })
 function stringValueReceived (stringReceived: string) {
-	
+    if (stringReceived == "Weapons Ready") {
+        serialWriter(0, stringReceived)
+    } else if (stringReceived == "Launched!") {
+        serialWriter(0, stringReceived)
+    } else if (stringReceived == "HeartFrogs") {
+        serialWriter(0, stringReceived)
+    } else {
+        serialWriter(0, "HeartFrogs")
+    }
 }
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(7)
+    radio.sendString("HeartFrogs")
+})
 radio.onReceivedString(function (receivedString) {
     stringValueReceived(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    radio.sendNumber(555)
 })
-input.onGesture(Gesture.Shake, function () {
-	
+input.onPinPressed(TouchPin.P1, function () {
+    radio.sendString("Launched!")
 })
+function serialWriter (num: number, text: string) {
+    if (text == "") {
+        serial.writeLine("" + (num))
+    } else {
+        serial.writeLine(text)
+    }
+}
 radio.setGroup(23)
